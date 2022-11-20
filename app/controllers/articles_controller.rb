7706968baa99a11b7c71ab32b7392fad94c4f4b7
Kinyua <alfkinyua@gmail.com>
2022-11-20 16:39:34 +0300

    Add articles controller with code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
+
 class ArticlesController < ActionController::Base
   def new
-    @article = Article.new
+    @art = Article.new
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    @art = Article.new(art_params)
+    @art.save
+    redirect_to articles_show(@art)
+  end
+
+  private
+
+  def art_params
+    params.require(:article).permit(:title, :description)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Student < ApplicationRecord
-    validates :name, presence: true, length:{minimum: 3, maximum: 10 }
-    validates :age, presence: true, length: {minimum: 1, maximum:3}
+  validates :name, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :age, presence: true, length: { minimum: 1, maximum: 3 }
 end
