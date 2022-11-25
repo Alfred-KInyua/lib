a8553c716cd4f446190b969843d7c038c0403aa4
Kinyua <alfkinyua@gmail.com>
2022-11-25 17:58:14 +0300

    Add list all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,34 +1,40 @@
 # frozen_string_literal: true
 
 class ArticlesController < ActionController::Base
+  def index
+    @articles = Article.all
+  end
+
   def new
     @art = Article.new
   end
-  def edit 
+
+  def edit
     @art = Article.find(params[:id])
   end
-  
 
   def create
     # render plain: params[:article].inspect
     @art = Article.new(art_params)
     if @art.save
       flash[:notice] = 'Your article was created successfully'
-      redirect_to article_path(@art) # this is the show template 
+      redirect_to article_path(@art) # this is the show template
     else
-      render :new   
-     
+      render :new
+
     end
   end
+
   def update
-    @art= Article.find(params[:id])
-   if @art.update(art_params)
-    flash[:notice] ='File updated successfully'
-    redirect_to article_path(@art)
-   else
- render :edit
-   end
+    @art = Article.find(params[:id])
+    if @art.update(art_params)
+      flash.notice = 'File updated successfully'
+      redirect_to article_path(@art)
+    else
+      render :edit
+    end
   end
+
   def show
     @art = Article.find(params[:id])
   end
